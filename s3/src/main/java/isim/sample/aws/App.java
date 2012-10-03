2674a492dbcd67db4aeb39d0952edb9eb2fcaf75
ivan.sim <ivan.sim@aeroinfo.com>
2012-10-03 10:12:17 -0700

    Add s3 project

---
+++ s3/src/main/java/isim/sample/aws/App.java
@@ -0,0 +1,13 @@
+package isim.sample.aws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
